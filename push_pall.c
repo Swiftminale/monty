76fd37212147b8908c44c5bc764bea9d127b0efc
Swiftminale <minalefetene@gmail.com>
2024-01-16 02:14:11 -0800

    first commit

--- push_pall.c
+++ push_pall.c
@@ -1,11 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
 
 #define STACK_SIZE 1000
 
 int stack[STACK_SIZE];
-int top = -1; 
+int top = -1;
 
 void push(int value) {
     if (top == STACK_SIZE - 1) {
@@ -23,16 +22,6 @@ void pall() {
     }
 }
 
-
-void pint() {
-    if (top == -1) {
-        fprintf(stderr, "L%d: can't pint, stack empty\n", line_number);
-        exit(EXIT_FAILURE);
-    }
-
-    printf("%d\n", stack[top]);
-}
-
 int main() {
     char opcode[100];
     int value, line_number = 0;
@@ -42,20 +31,19 @@ int main() {
 
         if (strcmp(opcode, "push") == 0) {
             if (scanf("%d", &value) != 1) {
-                fprintf(stderr, "L%d: usage: push integer\n", line_number);
+                fprintf(stderr, "Error: L%d: usage: push integer\n", line_number);
                 exit(EXIT_FAILURE);
             }
 
             push(value);
         } else if (strcmp(opcode, "pall") == 0) {
             pall();
-        } else if (strcmp(opcode, "pint") == 0) {
-            pint();
         } else {
-            fprintf(stderr, "L%d: unknown opcode %s\n", line_number, opcode);
+            fprintf(stderr, "Error: L%d: unknown opcode %s\n", line_number, opcode);
             exit(EXIT_FAILURE);
         }
     }
 
     return 0;
 }
+
